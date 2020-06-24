cf7cc99cca3c1c8548072853aaa93abb55a774d4
peirong3 <wupeirong294@gmail.com>
2020-06-24 02:12:21 +0000

    Tidy up index.js

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
-import React from 'react';
-
-import { addData } from './src/API';
-import InvestigateExplorer from './src/InvestigateExplorer';
-
-const Investigate = props => {
-  const { tabs, score } = props;
-
-  return (    
-    <InvestigateExplorer tabs={tabs} score={score} />    
-  );
-};
-
 export * from './src/redux';
-export { addData };
+export * from './src/API';
 
-export default Investigate;
+export { default as Investigate } from './src/InvestigateExplorer';
