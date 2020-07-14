2229c22d4e5200eaefbae8f1d09dfb0313462673
peirong3 <wupeirong294@gmail.com>
2020-07-14 04:12:46 +0000

    Remove unused Chart components

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -1,15 +1,4 @@
-import TSLineChart from './TSLineChart';
-import PieChart from './PieChart';
-import StatCards from './StatCards';
 import RiskBarChart from './RiskBarChart';
-import StackedBarChart from './StackedBarChart';
 import BarChart from './BarChart';
 
-export {
-  PieChart,
-  RiskBarChart,
-  TSLineChart,
-  StatCards,
-  StackedBarChart,
-  BarChart,
-};
+export { RiskBarChart, BarChart };
