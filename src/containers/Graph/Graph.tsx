2602034214559e2a3dbde0e9b7f5e679f33ff645
Timothy Lin <timothy.lin@cylynx.io>
2020-11-02 07:31:05 +0000

    fix: remove deselecting by clicking on node or edge

--- src/containers/Graph/Graph.tsx
+++ src/containers/Graph/Graph.tsx
@@ -7,7 +7,7 @@ import Graphin from '@antv/graphin';
 import { IG6GraphEvent } from '@antv/g6/lib/types';
 import * as Prop from '../../types/Prop';
 import activateRelations from './behaviors/activate-relations';
-import { setClickedId, getUI, getGraph } from '../../redux';
+import { setClickedId, getGraph } from '../../redux';
 import './graphin.css';
 // import '@antv/graphin/dist/index.css';
 
@@ -15,7 +15,6 @@ const Graph = React.forwardRef<HTMLDivElement, Prop.Graph>((props, ref) => {
   const { setTooltip } = props;
   const dispatch = useDispatch();
   const [, theme] = useStyletron();
-  const clickedId = useSelector((state) => getUI(state).clickedId);
   const graphVisible = useSelector((state) => getGraph(state).graphVisible);
   const layout = useSelector((state) => getGraph(state).styleOptions.layout);
 
@@ -41,20 +40,15 @@ const Graph = React.forwardRef<HTMLDivElement, Prop.Graph>((props, ref) => {
         const { centerX, centerY } = item.getBBox();
         const canvasXY = graph.getCanvasByPoint(centerX, centerY);
         const node = item.get('model');
-        if (clickedId === node.id) {
-          dispatch(setClickedId(null));
-          setTooltip(false);
-        } else {
-          dispatch(setClickedId(node.id));
-          setTooltip({
-            x: canvasXY.x,
-            y: canvasXY.y,
-            selected: {
-              type: 'node',
-              obj: node,
-            },
-          });
-        }
+        dispatch(setClickedId(node.id));
+        setTooltip({
+          x: canvasXY.x,
+          y: canvasXY.y,
+          selected: {
+            type: 'node',
+            obj: node,
+          },
+        });
       }
     };
 
@@ -68,20 +62,15 @@ const Graph = React.forwardRef<HTMLDivElement, Prop.Graph>((props, ref) => {
         const { centerX, centerY } = item.getBBox();
         const canvasXY = graph.getCanvasByPoint(centerX, centerY);
         const edge = item.get('model');
-        if (clickedId === edge.id) {
-          dispatch(setClickedId(null));
-          setTooltip(false);
-        } else {
-          dispatch(setClickedId(edge.id));
-          setTooltip({
-            x: canvasXY.x,
-            y: canvasXY.y,
-            selected: {
-              type: 'edge',
-              obj: edge,
-            },
-          });
-        }
+        dispatch(setClickedId(edge.id));
+        setTooltip({
+          x: canvasXY.x,
+          y: canvasXY.y,
+          selected: {
+            type: 'edge',
+            obj: edge,
+          },
+        });
       }
     };
 
@@ -98,7 +87,7 @@ const Graph = React.forwardRef<HTMLDivElement, Prop.Graph>((props, ref) => {
       graph.off('canvas:click', onResetClick);
       graph.off('canvas:drag', onResetClick);
     };
-  }, [clickedId, dispatch, setTooltip]);
+  }, [dispatch, setTooltip]);
 
   return (
     <Graphin
