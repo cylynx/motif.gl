84e054747100a1a06534f3d7649308a08f1b507a
Timothy Lin <timothy.lin@cylynx.io>
2020-11-15 11:19:53 +0000

    test: add test for graph-slice

--- src/redux/graph-slice.test.js
+++ src/redux/graph-slice.test.js
@@ -1,116 +1,142 @@
-import graph, { addQuery, updateGraph, timeRangeChange } from './graph-slice';
-import testNeighbourData from '../../constants/testNeighbourData.json';
-import testNeighbourData2 from '../../constants/testNeighbourData2.json';
+import graph, {
+  initialState,
+  addQuery,
+  processGraphResponse,
+  updateGraphList,
+  deleteGraphList,
+  changeVisibilityGraphList,
+} from './graph-slice';
+import { importJson } from '../processors/import-data';
 
-const initialState = {
-  graphList: [],
-  graphFlatten: [],
-  timeRange: [],
-  graphVisible: [],
+const JsonData = {
+  nodes: [{ id: 'node-1' }, { id: 'node-2' }],
+  edges: [{ id: 'edge-1', source: 'node-1', target: 'node-2' }],
+  metadata: {
+    key: 123,
+  },
 };
 
-describe('graph reducer', () => {
-  // it('should handle initial state', () => {
-  //   expect(graph(undefined, {})).toEqual(initialState);
-  // });
+const JsonData2 = {
+  nodes: [{ id: 'node-2' }, { id: 'node-3' }],
+  edges: [{ id: 'edge-2', source: 'node-2', target: 'node-3' }],
+  metadata: {
+    key: 234,
+  },
+};
 
-  it('should handle addQuery', () => {
+const sampleGraphList = importJson(JsonData, initialState.accessors);
+
+const sampleGraphList2 = importJson(JsonData2, initialState.accessors);
+
+describe('graph reducer', () => {
+  it('should handle initial state', () => {
+    expect(graph(undefined, {})).toEqual(initialState);
+  });
+
+  it('addQuery should add to graphList', async () => {
+    const sampleGraphData = (await sampleGraphList)[0];
     const results = graph(initialState, {
       type: addQuery.type,
-      payload: testNeighbourData,
+      payload: sampleGraphData,
     });
     expect(results.graphList).toHaveLength(1);
-    expect(results.graphList).toEqual([testNeighbourData]);
+    expect(results.graphList).toEqual([sampleGraphData]);
   });
 
-  it('should update graph when given new nodes and edges array', () => {
-    const results = graph(
-      {
-        graphFlatten: {
-          nodes: [{ id: 'n1' }],
-          edges: [{ id: 'e1' }],
-        },
+  it('processGraphResponse should add to graphFlatten and change graphVisible', async () => {
+    const sampleGraphData = (await sampleGraphList)[0];
+    const results = graph(initialState, {
+      type: processGraphResponse.type,
+      payload: {
+        data: sampleGraphData,
+        accessors: initialState.accessors,
       },
-      {
-        type: updateGraph.type,
-        payload: {
-          graphName: 'graphFlatten',
-          newNodes: [{ id: 'n2' }],
-          newEdges: [{ id: 'e2' }],
-        },
-      }
-    );
-    expect(results.graphFlatten).toEqual({
-      nodes: [{ id: 'n2' }],
-      edges: [{ id: 'e2' }],
     });
+
+    expect(results.graphFlatten.nodes).toHaveLength(JsonData.nodes.length);
+    expect(results.graphFlatten.edges).toHaveLength(JsonData.edges.length);
+    expect(results.graphVisible.nodes).toHaveLength(JsonData.nodes.length);
+    expect(results.graphVisible.edges).toHaveLength(JsonData.edges.length);
   });
 
-  it('should not update node/edge if given empty array', () => {
-    const results = graph(
-      {
-        graphFlatten: {
-          nodes: [{ id: 'n1' }],
-          edges: [{ id: 'e1' }],
-        },
+  it('processGraphResponse should contain only unique ids', async () => {
+    const sampleGraphData = (await sampleGraphList)[0];
+    const sampleGraphData2 = (await sampleGraphList2)[0];
+    let results = graph(initialState, {
+      type: processGraphResponse.type,
+      payload: {
+        data: sampleGraphData,
+        accessors: initialState.accessors,
       },
-      {
-        type: updateGraph.type,
-        payload: {
-          graphName: 'graphFlatten',
-          newNodes: [{ id: 'n2' }],
-          newEdges: [],
-        },
-      }
-    );
-    expect(results.graphFlatten).toEqual({
-      nodes: [{ id: 'n2' }],
-      edges: [{ id: 'e1' }],
     });
+    results = graph(results, {
+      type: processGraphResponse.type,
+      payload: {
+        data: sampleGraphData2,
+        accessors: initialState.accessors,
+      },
+    });
+
+    expect(results.graphFlatten.nodes).toHaveLength(3);
+    expect(results.graphFlatten.edges).toHaveLength(2);
+    expect(results.graphVisible.nodes).toHaveLength(3);
+    expect(results.graphVisible.edges).toHaveLength(2);
   });
 
-  it('should not update node/edge if missing', () => {
-    const results = graph(
-      {
-        graphFlatten: {
-          nodes: [{ id: 'n1' }],
-          edges: [{ id: 'e1' }],
-        },
+  it('updateGraphList should switch order of GraphList', async () => {
+    const sampleGraphData = (await sampleGraphList)[0];
+    const sampleGraphData2 = (await sampleGraphList2)[0];
+    const modGraphList = { graphList: [sampleGraphData, sampleGraphData2] };
+    const modState = { ...initialState, ...modGraphList };
+    const results = graph(modState, {
+      type: updateGraphList.type,
+      payload: {
+        from: 0,
+        to: 1,
       },
-      {
-        type: updateGraph.type,
-        payload: {
-          graphName: 'graphFlatten',
-          newNodes: [{ id: 'n2' }],
-        },
-      }
-    );
-    expect(results.graphFlatten).toEqual({
-      nodes: [{ id: 'n2' }],
-      edges: [{ id: 'e1' }],
     });
+
+    expect(results.graphList).toHaveLength(2);
+    expect(results.graphList).toEqual([sampleGraphData2, sampleGraphData]);
   });
 
-  it('should filter timerange correctly', () => {
-    const results = graph(
-      {
-        graphVisible: {},
-        graphFlatten: {
-          nodes: [{ id: 'n1' }, { id: 'n2' }, { id: 'n3' }],
-          edges: [
-            { id: 'e1', from: 'n1', to: 'n2', data: { blk_ts_unix: 3 } },
-            { id: 'e1', from: 'n2', to: 'n3', data: { blk_ts_unix: 6 } },
-          ],
-        },
+  it('deleteGraphList should reset graphVisible and remove the given list', async () => {
+    const sampleGraphData = (await sampleGraphList)[0];
+    const modGraphList = {
+      graphList: [sampleGraphData],
+      graphVisible: {
+        nodes: [...sampleGraphData.nodes],
+        edges: [...sampleGraphData.edges],
       },
-      {
-        type: timeRangeChange.type,
-        payload: [5, 10],
-      }
-    );
-    expect(results.graphVisible).toEqual({
-      nodes: [{ id: 'n2' }, { id: 'n3' }],
-      edges: [{ id: 'e1', from: 'n2', to: 'n3', data: { blk_ts_unix: 6 } }],
+    };
+    const modState = { ...initialState, ...modGraphList };
+    const results = graph(modState, {
+      type: deleteGraphList.type,
+      payload: 0,
     });
+
+    expect(results.graphList).toHaveLength(0);
+    expect(results.graphVisible.nodes).toEqual([]);
+    expect(results.graphVisible.edges).toEqual([]);
+  });
+
+  it('changeVisibilityGraphList should reset graphVisible and but not delete from list', async () => {
+    const sampleGraphData = (await sampleGraphList)[0];
+    const modGraphList = {
+      graphList: [sampleGraphData],
+      graphVisible: {
+        nodes: [...sampleGraphData.nodes],
+        edges: [...sampleGraphData.edges],
+      },
+    };
+    const modState = { ...initialState, ...modGraphList };
+    const results = graph(modState, {
+      type: changeVisibilityGraphList.type,
+      payload: { index: 0, isVisible: false },
+    });
+
+    expect(results.graphList).toHaveLength(1);
+    expect(results.graphVisible.nodes).toEqual([]);
+    expect(results.graphVisible.edges).toEqual([]);
   });
 });
