9398b2327dd87c8fe64d6466bcb679ef9e6e0ec2
Timothy Lin <timothy.lin@cylynx.io>
2020-11-17 11:18:17 +0000

    feat: add legend color mapping to toolbar

--- src/containers/Toolbar/LegendPopover.tsx
+++ src/containers/Toolbar/LegendPopover.tsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { useStyletron } from 'baseui';
 import { useDispatch, useSelector } from 'react-redux';
 import { Block } from 'baseui/block';
+import { LabelSmall } from 'baseui/typography';
 import { changeNodeStyle } from '../../redux/graph-slice';
 import { getGraph } from '../../redux';
 import { NestedForm, genNestedForm } from '../../components/form';
 import { nodeColorForm } from '../SidePanel/OptionsPanel';
 import { getFieldNames } from '../../utils/graph-utils';
+
+const MAX_LEGEND_SIZE = 8;
+
+const Legend = ({ data }: { [key: string]: string }) => {
+  const [css] = useStyletron();
+  let valueArr = Object.keys(data);
+  let colorArr = Object.values(data);
+  if (valueArr.length > MAX_LEGEND_SIZE) {
+    valueArr = valueArr.slice(0, MAX_LEGEND_SIZE);
+    valueArr.push('Others');
+    colorArr = colorArr.slice(0, MAX_LEGEND_SIZE);
+    colorArr.push(colorArr[MAX_LEGEND_SIZE - 1]);
+  }
+  return (
+    <Fragment>
+      {valueArr.map((value, i) => (
+        <Block key={value} display='flex' alignItems='center'>
+          <div
+            className={css({
+              height: '18px',
+              width: '18px',
+              marginRight: '16px',
+              marginLeft: '8px',
+              marginTop: '8px',
+              marginBottom: '8px',
+              backgroundColor: colorArr[i],
+              borderRadius: '50%',
+            })}
+          />
+          <LabelSmall>{value}</LabelSmall>
+        </Block>
+      ))}
+    </Fragment>
+  );
+};
 
 const LegendPopover = () => {
   const dispatch = useDispatch();
@@ -23,15 +60,17 @@ const LegendPopover = () => {
   const updateNodeStyle = (data: any) => dispatch(changeNodeStyle(data));
 
   return (
-    <div style={{ width: '300px' }}>
-      <Block padding='10px'>
-        <NestedForm
-          data={genNestedForm(nodeColorForm, nodeStyle, updateNodeStyle, {
-            'legend[0].options': nodeOptions,
-          })}
-        />
-      </Block>
-    </div>
+    <Block padding='10px' width='300px'>
+      <NestedForm
+        data={genNestedForm(nodeColorForm, nodeStyle, updateNodeStyle, {
+          'legend[0].options': nodeOptions,
+        })}
+      />
+      {nodeStyle.color.id === 'legend' && nodeStyle.color.mapping && (
+        // @ts-ignore
+        <Legend data={nodeStyle.color.mapping} />
+      )}
+    </Block>
   );
 };
 export default LegendPopover;
