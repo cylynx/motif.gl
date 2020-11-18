cac26f17a50dfdf9ec9a43f0173185ba4a836126
Timothy Lin <timothy.lin@cylynx.io>
2020-11-18 15:06:50 +0000

    feat: add header control buttons

---
+++ src/containers/SidePanel/HeaderButton.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react/destructuring-assignment */
+import React, { Fragment } from 'react';
+import { Button } from 'baseui/button';
+import { StatefulPopover, PLACEMENT, TRIGGER_TYPE } from 'baseui/popover';
+import { LabelSmall } from 'baseui/typography';
+import { Block } from 'baseui/block';
+
+export type HeaderButtonProp = {
+  key: number;
+  name: string;
+  icon: React.ReactNode;
+  isDisabled: boolean;
+  onClick: () => any;
+};
+
+const HeaderButton = (props: HeaderButtonProp) => {
+  const { name, icon, isDisabled, onClick } = props;
+
+  return (
+    <Fragment>
+      <StatefulPopover
+        content={<LabelSmall padding='scale200'>{name}</LabelSmall>}
+        placement={PLACEMENT.bottom}
+        showArrow
+        triggerType={TRIGGER_TYPE.hover}
+      >
+        <Button
+          $as='div'
+          onClick={onClick}
+          disabled={isDisabled}
+          kind='tertiary'
+          size='mini'
+        >
+          {icon}
+        </Button>
+      </StatefulPopover>
+    </Fragment>
+  );
+};
+
+export default HeaderButton;
