c8019119d6b8863ab3fc8e6853237f5468e197ac
Timothy Lin <timothy.lin@cylynx.io>
2020-11-19 13:37:53 +0000

    fix: add node and edge property overwrite

--- src/containers/Toolbar/SettingsPopover.tsx
+++ src/containers/Toolbar/SettingsPopover.tsx
@@ -11,19 +11,35 @@ import {
   changeNodeStyle,
   changeEdgeStyle,
 } from '../../redux/graph-slice';
-import { getGraph } from '../../redux';
+import { getGraph, getStyleOptions } from '../../redux';
+import { getFieldNames } from '../../utils/graph-utils';
 import * as LAYOUT from '../../constants/layout-options';
 import { NestedForm, genNestedForm } from '../../components/form';
 import { nodeSizeForm, edgeWidthForm } from '../SidePanel/OptionsPanel';
 
 const SettingsPopover = () => {
   const dispatch = useDispatch();
-  const { nodeStyle, edgeStyle, resetView, groupEdges } = useSelector(
-    (state) => getGraph(state).styleOptions,
+  const {
+    nodeStyle,
+    edgeStyle,
+    resetView,
+    groupEdges,
+    layout,
+  } = useSelector((state) => getStyleOptions(state));
+  const graphFields = useSelector(
+    (state) => getGraph(state).graphFlatten.metadata.fields,
   );
-  const layoutName = useSelector(
-    (state) => getGraph(state).styleOptions.layout.name,
-  );
+  const numericNodeOptions = getFieldNames(graphFields.nodes, [
+    'integer',
+    'real',
+  ]).map((x) => {
+    return { id: x, label: x };
+  });
+  const numericEdgeOptions =
+    getFieldNames(graphFields.edges, ['integer', 'real']).map((x) => {
+      return { id: x, label: x };
+    }) || [];
+
   const findID = (options: { label: string; id: string }[], id: string) =>
     options.find((x) => x.id === id);
 
@@ -44,7 +60,7 @@ const SettingsPopover = () => {
           options={LAYOUT.LAYOUT_NAMES}
           size='compact'
           clearable={false}
-          value={[findID(LAYOUT.LAYOUT_NAMES, layoutName)]}
+          value={[findID(LAYOUT.LAYOUT_NAMES, layout.name)]}
           onChange={(params) =>
             dispatch(
               changeLayout({ layout: { id: params.option.id as string } }),
@@ -54,11 +70,18 @@ const SettingsPopover = () => {
       </FormControl>
       <NestedForm
         data={genNestedForm(nodeSizeForm, nodeStyle, updateNodeStyle, {
+          'property[0].options': numericNodeOptions,
+          'property[0].value':
+            numericNodeOptions.length > 0 ? numericNodeOptions[0].id : null,
+
           labelPosition: 'top',
         })}
       />
       <NestedForm
         data={genNestedForm(edgeWidthForm, edgeStyle, updateEdgeStyle, {
+          'property[0].options': numericEdgeOptions,
+          'property[0].value':
+            numericEdgeOptions.length > 0 ? numericEdgeOptions[0].id : null,
           labelPosition: 'top',
         })}
       />
