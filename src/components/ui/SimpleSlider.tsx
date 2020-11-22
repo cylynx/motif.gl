df21dd996367310781759e937e58aafc0496e7b0
Timothy Lin <timothy.lin@cylynx.io>
2020-11-22 03:54:47 +0000

    feat: redesign and expose more props

--- src/components/ui/SimpleSlider.tsx
+++ src/components/ui/SimpleSlider.tsx
@@ -1,10 +1,23 @@
 import * as React from 'react';
-import { Slider as BaseSlider, SliderProps } from 'baseui/slider';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { getTrackBackground } from 'react-range';
+import { useStyletron } from 'baseui';
+import {
+  Slider as BaseSlider,
+  SliderProps as BaseSliderProps,
+} from 'baseui/slider';
+
+interface SliderProps extends BaseSliderProps {
+  showThumbValue?: boolean;
+  showTickBar?: boolean;
+}
 
 const SimpleSlider = (props: SliderProps) => {
+  const { showThumbValue = true, showTickBar = true, ...rest } = props;
+  const [css, theme] = useStyletron();
   return (
     <BaseSlider
-      {...props}
+      {...rest}
       overrides={{
         Thumb: {
           style: ({ $theme, $value, $thumbIndex }) => {
@@ -17,25 +30,17 @@ const SimpleSlider = (props: SliderProps) => {
             }
             return {
               height: '12px',
-              width: isLeft || isRight ? '6px' : '12px',
-              borderTopLeftRadius: isRight ? '1px' : '4px',
-              borderTopRightRadius: isLeft ? '1px' : '4px',
-              borderBottomLeftRadius: isRight ? '1px' : '4px',
-              borderBottomRightRadius: isLeft ? '1px' : '4px',
+              width: '12px',
+              borderTopLeftRadius: 0,
+              borderTopRightRadius: 0,
+              borderBottomLeftRadius: 0,
+              borderBottomRightRadius: 0,
               backgroundColor: $theme.colors.primary,
               color: $theme.colors.contentPrimary,
               display: 'flex',
               outline: 'none',
               justifyContent: 'center',
               alignItems: 'center',
-              borderLeftWidth: '1px',
-              borderRightWidth: '1px',
-              borderTopWidth: '1px',
-              borderBottomWidth: '1px',
-              borderLeftStyle: 'solid',
-              borderRightStyle: 'solid',
-              borderTopStyle: 'solid',
-              borderBottomStyle: 'solid',
               borderLeftColor: $theme.colors.mono400,
               borderRightColor: $theme.colors.mono400,
               borderTopColor: $theme.colors.mono400,
@@ -46,6 +51,7 @@ const SimpleSlider = (props: SliderProps) => {
         ThumbValue: {
           style: ({ $theme }) => {
             return {
+              display: showThumbValue ? 'block' : 'none',
               position: 'absolute',
               backgroundColor: 'transparent',
               top: `-${$theme.sizing.scale700}`,
@@ -63,9 +69,37 @@ const SimpleSlider = (props: SliderProps) => {
           },
         },
         InnerTrack: {
-          style: () => {
+          style: ({ $theme, $value = [], $min, $max, $disabled }) => {
+            const { colors, direction } = $theme;
             return {
-              height: '2px',
+              height: '3px',
+              background: getTrackBackground({
+                values: $value,
+                colors:
+                  $value.length === 1
+                    ? [
+                        $disabled
+                          ? colors.borderOpaque
+                          : colors.contentTertiary,
+                        $disabled
+                          ? colors.backgroundSecondary
+                          : colors.borderOpaque,
+                      ]
+                    : [
+                        $disabled
+                          ? colors.backgroundSecondary
+                          : colors.borderOpaque,
+                        $disabled
+                          ? colors.borderOpaque
+                          : colors.contentTertiary,
+                        $disabled
+                          ? colors.backgroundSecondary
+                          : colors.borderOpaque,
+                      ],
+                min: $min || 0,
+                max: $max || 0,
+                rtl: direction === 'rtl',
+              }),
             };
           },
         },
@@ -73,16 +107,25 @@ const SimpleSlider = (props: SliderProps) => {
           style: ({ $theme }) => {
             return {
               paddingBottom: $theme.sizing.scale200,
+              paddingLeft: 0,
+              paddingRight: 0,
             };
           },
         },
-        TickBar: {
-          style: () => {
-            return {
-              paddingBottom: 0,
-            };
-          },
-        },
+        TickBar: ({ $min, $max }) => (
+          <div
+            className={css({
+              display: showTickBar ? 'flex' : 'none',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+              color: theme.colors.contentPrimary,
+              ...theme.typography.font200,
+            })}
+          >
+            <div>{$min}</div>
+            <div>{$max}</div>
+          </div>
+        ),
         InnerThumb: () => null,
       }}
     />
