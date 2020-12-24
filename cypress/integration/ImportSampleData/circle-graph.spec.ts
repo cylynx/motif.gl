161e6a0c96c2f38a3ecd1c8a5435ad7c6f2d012b
Yinghua Chai <yinghuachai0@gmail.com>
2020-12-24 16:31:42 +0800

    chore: commit something to trigger github actions

--- cypress/integration/ImportSampleData/circle-graph.spec.ts
+++ cypress/integration/ImportSampleData/circle-graph.spec.ts
@@ -3,7 +3,7 @@ import 'cypress-react-selector';
 import { SampleData } from '../../../src/containers/ImportWizard/ImportSampleData/ImportSampleData';
 import { DndItem } from '../../../src/components/DndList';
 
-describe('Import Circle Graphs', () => {
+describe('Import Circle Graph', () => {
   before(() => {
     cy.visit('/');
     cy.waitForReact();
