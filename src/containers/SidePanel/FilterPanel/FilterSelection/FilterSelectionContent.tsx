ad57611a37074e6fa8b287e710224a41522c4432
Yinghua Chai <yinghuachai0@gmail.com>
2020-12-29 10:26:17 +0800

    feat: content for filter selection

---
+++ src/containers/SidePanel/FilterPanel/FilterSelection/FilterSelectionContent.tsx
@@ -0,0 +1,9 @@
+import React, { FC } from 'react';
+
+type FilterSelectionContentProps = {};
+
+const FilterSelectionContent: FC<FilterSelectionContentProps> = () => {
+  return <div>Filter Selection Content</div>;
+};
+
+export default FilterSelectionContent;
