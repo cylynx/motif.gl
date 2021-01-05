8b329e4f3dbc58b2fbed07b7c28d9cfb358bacfe
Yinghua Chai <yinghuachai0@gmail.com>
2021-01-05 19:04:35 +0800

    test: applied unit test on implementation

--- src/redux/combine-reducers.test.ts
+++ src/redux/combine-reducers.test.ts
@@ -1,6 +1,5 @@
 // @ts-nocheck
 import { combineReducers } from '@reduxjs/toolkit';
-import { Graph } from '@antv/graphin/dist';
 import { initialState as initialStateGraph } from './graph-slice';
 import { initialStateUi } from './ui-slice';
 import {
@@ -12,7 +11,11 @@ import {
   getGraphFlatten,
   getGraphVisible,
   getFilterOptions,
+  getGraphFiltered,
+  getAccessors,
+  getWidget,
 } from './combine-reducers';
+import { FilterOptions, GraphData, Node } from '../containers/Graph';
 
 const clientReducer = combineReducers({
   investigate: investigateReducer,
@@ -23,6 +26,7 @@ describe('selectors', () => {
     const results = clientReducer({}, {});
     expect(getUI(results)).toEqual(initialStateUi);
     expect(getGraph(results)).toEqual(initialStateGraph);
+    expect(getAccessors(results)).toEqual(initialStateGraph.accessors);
     expect(getGraphList(results)).toEqual(initialStateGraph.graphList);
     expect(getGraphFlatten(results)).toEqual(initialStateGraph.graphFlatten);
     expect(getStyleOptions(results)).toEqual(initialStateGraph.styleOptions);
@@ -31,7 +35,7 @@ describe('selectors', () => {
   });
 
   describe('getGraphFiltered', () => {
-    const mockGraphFlatten: Graph.GraphData = {
+    const mockGraphFlatten: GraphData = {
       nodes: [
         { id: 'node-1' },
         { id: 'node-2' },
@@ -76,10 +80,130 @@ describe('selectors', () => {
       },
     };
 
-    const mockGraphFiltered = {
-      firstKey: {},
-    };
+    it('should filter node properties', () => {
+      const filterNodeOption: FilterOptions = {
+        firstKey: {
+          id: 'id',
+          from: 'nodes',
+          selection: [
+            {
+              id: 'id',
+              label: 'id',
+              type: 'string',
+              analyzerType: 'STRING',
+              format: '',
+              from: 'nodes',
+              optionKey: 'nodes-id',
+              __optgroup: 'Nodes',
+            },
+          ],
+          analyzerType: 'STRING',
+          isFilterReady: true,
+          stringOptions: [
+            {
+              id: 'node-1',
+              label: 'node-1',
+            },
+            {
+              id: 'node-2',
+              label: 'node-2',
+            },
+            {
+              id: 'node-3',
+              label: 'node-3',
+            },
+            {
+              id: 'node-4',
+              label: 'node-4',
+            },
+          ],
+          caseSearch: [
+            {
+              id: 'node-1',
+              label: 'node-1',
+            },
+          ],
+        },
+      };
 
-    it('getGraphFiltered should filter node properties', () => {});
+      const reducer = clientReducer(
+        {
+          investigate: {
+            graph: {
+              graphFlatten: mockGraphFlatten,
+              filterOptions: filterNodeOption,
+            },
+          },
+        },
+        {},
+      );
+      const results: GraphData = getGraphFiltered(reducer);
+      const { nodes } = results;
+      expect(nodes[0].id).toEqual('node-1');
+      expect(nodes.length).toEqual(1);
+    });
+
+    it('should filter edge properties', () => {
+      const filterEdgeOption: FilterOptions = {
+        secondKey: {
+          id: 'id',
+          from: 'edges',
+          selection: [
+            {
+              id: 'id',
+              label: 'id',
+              type: 'string',
+              analyzerType: 'STRING',
+              format: '',
+              from: 'edges',
+              optionKey: 'edges-id',
+              __optgroup: 'Edges',
+            },
+          ],
+          analyzerType: 'STRING',
+          isFilterReady: true,
+          stringOptions: [
+            {
+              id: 'edge-1',
+              label: 'edge-1',
+            },
+            {
+              id: 'edge-2',
+              label: 'edge-2',
+            },
+          ],
+          caseSearch: [
+            {
+              id: 'edge-1',
+              label: 'edge-1',
+            },
+          ],
+        },
+      };
+
+      const reducer = clientReducer(
+        {
+          investigate: {
+            graph: {
+              present: {
+                graphFlatten: mockGraphFlatten,
+                filterOptions: filterEdgeOption,
+              },
+            },
+          },
+        },
+        {},
+      );
+
+      const results: GraphData = getGraphFiltered(reducer);
+      const { nodes, edges } = results;
+
+      const isNodeIdsPresent: boolean = nodes.every((node: Node) =>
+        ['node-1', 'node-2'].includes(node.id),
+      );
+
+      expect(isNodeIdsPresent).toBe(true);
+      expect(edges[0].id).toBe('edge-1');
+    });
   });
 });
