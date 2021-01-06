b68f998ad32fea988ff9de2e243e2a86c1568fc5
Yinghua Chai <yinghuachai0@gmail.com>
2021-01-06 19:10:07 +0800

    test: histgram filters

--- cypress/integration/FilterPanel/histogram-filters.spec.ts
+++ cypress/integration/FilterPanel/histogram-filters.spec.ts
@@ -3,7 +3,7 @@ import 'cypress-react-selector';
 import { SampleData } from '../../../src/containers/ImportWizard/ImportSampleData/ImportSampleData';
 
 describe('Histogram Filters', () => {
-  before(() => {
+  beforeEach(() => {
     cy.visit('/');
     cy.waitForReact();
 
@@ -16,9 +16,7 @@ describe('Histogram Filters', () => {
     })
       .find('Button')
       .click();
-  });
 
-  beforeEach(() => {
     // switch to filter panel
     cy.react('Block', {
       props: {
@@ -64,7 +62,93 @@ describe('Histogram Filters', () => {
 
     // results
     cy.getReact('Graph').getProps('data.nodes').should('have.length', 7);
-
     cy.getReact('Graph').getProps('data.edges').should('have.length', 0);
+  });
+
+  it('should filter with DateTime Variable', () => {
+    // select variable
+    cy.react('FilterSelection')
+      .react('Header')
+      .react('SelectVariable', {
+        props: {
+          'data-testid': 'filter-selection-header:select-variable',
+        },
+      })
+      .first()
+      .type('create_date{enter}');
+
+    // switch to layer panel
+    cy.react('Block', {
+      props: {
+        'data-testid': 'layers',
+      },
+      exact: true,
+    })
+      .react('IconButton', {
+        props: { id: 'layers', group: 'main' },
+      })
+      .click();
+
+    // results
+    cy.getReact('Graph').getProps('data.nodes').should('have.length', 9);
+    cy.getReact('Graph').getProps('data.edges').should('have.length', 14);
+  });
+
+  it('should filter with Date Variable', () => {
+    // select variable
+    cy.react('FilterSelection')
+      .react('Header')
+      .react('SelectVariable', {
+        props: {
+          'data-testid': 'filter-selection-header:select-variable',
+        },
+      })
+      .first()
+      .type('date{enter}');
+
+    // switch to layer panel
+    cy.react('Block', {
+      props: {
+        'data-testid': 'layers',
+      },
+      exact: true,
+    })
+      .react('IconButton', {
+        props: { id: 'layers', group: 'main' },
+      })
+      .click();
+
+    // results
+    cy.getReact('Graph').getProps('data.nodes').should('have.length', 9);
+    cy.getReact('Graph').getProps('data.edges').should('have.length', 14);
+  });
+
+  it('should filter with Time Variable', () => {
+    // select variable
+    cy.react('FilterSelection')
+      .react('Header')
+      .react('SelectVariable', {
+        props: {
+          'data-testid': 'filter-selection-header:select-variable',
+        },
+      })
+      .first()
+      .type('time{enter}');
+
+    // switch to layer panel
+    cy.react('Block', {
+      props: {
+        'data-testid': 'layers',
+      },
+      exact: true,
+    })
+      .react('IconButton', {
+        props: { id: 'layers', group: 'main' },
+      })
+      .click();
+
+    // results
+    cy.getReact('Graph').getProps('data.nodes').should('have.length', 9);
+    cy.getReact('Graph').getProps('data.edges').should('have.length', 14);
   });
 });
