fa55f1d251ff70d487afd17f8726b31fc16fea50
Yinghua Chai <yinghuachai0@gmail.com>
2021-02-08 22:08:42 +0800

    build: setup webpack loader for cypress

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -1,8 +1,18 @@
-import Cypress from 'cypress';
+import * as Cypress from 'cypress';
+import webpackPreprocessor from '@cypress/webpack-preprocessor';
 
 /**
- * @type {Cypress.PluginConfig}
+ * @param {Cypress.PluginEvents} on
+ * @param {Cypress.PluginConfigOptions} config
  */
-export default (_: any, config: any) => {
+export default (
+  on: Cypress.PluginEvents,
+  config: Cypress.PluginConfigOptions,
+) => {
+  const options = {
+    webpackOptions: require('../webpack.config.babel'),
+    watchOptions: {},
+  };
+  on('file:preprocessor', webpackPreprocessor(options));
   return config;
 };
---
+++ cypress/webpack.config.babel.js
@@ -0,0 +1,47 @@
+const config = {
+  resolve: {
+    extensions: ['.tsx', '.ts', '.css', '.js', '.jsx', '.less'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-transform-runtime'],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              // skip typechecking for speed
+              transpileOnly: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(css)$/i,
+        exclude: [/node_modules/],
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/i,
+        exclude: [/node_modules/],
+        use: ['less-loader'],
+      },
+    ],
+  },
+};
+
+module.exports = config;
