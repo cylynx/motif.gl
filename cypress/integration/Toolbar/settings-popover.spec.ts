30ed623f12330d70d091bd7abf4c5a1df332ae9b
Yinghua Chai <yinghuachai0@gmail.com>
2021-02-11 14:48:31 +0800

    test(toolbar): setting popover

--- cypress/integration/Toolbar/settings-popover.spec.ts
+++ cypress/integration/Toolbar/settings-popover.spec.ts
@@ -1,7 +1,56 @@
 import { SampleData } from '../../../src/containers/ImportWizard/ImportSampleData/ImportSampleData';
 import * as layoutOptions from '../../../src/constants/layout-options';
+import * as form from '../../../src/containers/SidePanel/OptionsPanel/constants';
+import {
+  EdgeStyleOptions,
+  EdgeWidthFixed,
+  EdgeWidthProperty,
+  GraphData,
+  GraphSelectors,
+  NodeSizeDegree,
+  NodeSizeFixed,
+  NodeSizeProperty,
+  NodeStyleOptions,
+} from '../../../src/redux/graph';
+import { NestedFormData } from '../../../src/components/form';
 
 describe('Setting Popovers', () => {
+  const findDefaultFromForm = (
+    form: NestedFormData,
+    layout: string,
+    controllerName: string,
+  ) => {
+    return form[layout].find((value: any) => value.id === controllerName);
+  };
+
+  const getEdgeStyleFromReduxStore = (): Promise<EdgeStyleOptions> => {
+    return new Promise((resolve) => {
+      cy.getReact('Provider')
+        .getProps('store')
+        .then((store) => {
+          const globalStore = store.getState();
+          const { edgeStyle } = GraphSelectors.getGraph(
+            globalStore,
+          ).styleOptions;
+          return resolve(edgeStyle);
+        });
+    });
+  };
+
+  const getNodeStyleFromReduxStore = (): Promise<NodeStyleOptions> => {
+    return new Promise((resolve) => {
+      cy.getReact('Provider')
+        .getProps('store')
+        .then((store) => {
+          const globalStore = store.getState();
+          const { nodeStyle } = GraphSelectors.getGraph(
+            globalStore,
+          ).styleOptions;
+          return resolve(nodeStyle);
+        });
+    });
+  };
+
   before(() => {
     cy.visit('/');
     cy.waitForReact(5000);
@@ -148,9 +197,139 @@ describe('Setting Popovers', () => {
     });
   });
 
-  describe('Node Size', () => {});
+  describe('Node Size', () => {
+    it('Fixed', async () => {
+      cy.react('Controller', { props: { name: 'size' } }).type('Fixed{enter}');
 
-  describe('Edge Width', () => {});
+      const modifyValue = 1;
+      const arrows = '{rightarrow}'.repeat(modifyValue);
+      const nodeSizeType = 'fixed';
+      const controllerName = 'value';
 
-  describe('Group Edge', () => {});
+      cy.react('SettingsPopover')
+        .react('NestedForm', {
+          props: { id: 'nodeSize' },
+        })
+        .react('Controller', { props: { name: controllerName } })
+        .nthNode(0)
+        .type(arrows);
+
+      const formDefaults = findDefaultFromForm(
+        form.nodeSizeForm,
+        nodeSizeType,
+        controllerName,
+      );
+      const { max } = formDefaults;
+
+      const nodeStyle = await getNodeStyleFromReduxStore();
+      const { id, value } = nodeStyle.size as NodeSizeFixed;
+      expect(id).to.deep.equal(nodeSizeType);
+      expect(value).to.deep.equal(max / 2 + 2);
+    });
+
+    it('Degree of Connections', async () => {
+      cy.react('Controller', { props: { name: 'size' } }).type('Degree{enter}');
+
+      const nodeSizeType = 'degree';
+      const controllerName = 'range';
+
+      const formDefaults = findDefaultFromForm(
+        form.nodeSizeForm,
+        nodeSizeType,
+        controllerName,
+      );
+      const { value } = formDefaults;
+
+      const nodeStyle = await getNodeStyleFromReduxStore();
+      const { id, range } = nodeStyle.size as NodeSizeDegree;
+      expect(id).to.deep.equal(nodeSizeType);
+      expect(range).to.deep.equal(value);
+    });
+
+    it('Property (User Defined)', async () => {
+      const nodeSizeType = 'property';
+      const controllerName = 'range';
+
+      cy.react('Controller', { props: { name: 'size' } }).type(
+        'Property{enter}',
+      );
+      cy.react('Controller', { props: { name: 'variable' } }).type(
+        'risk_score{enter}',
+      );
+
+      const formDefaults = findDefaultFromForm(
+        form.nodeSizeForm,
+        nodeSizeType,
+        controllerName,
+      );
+      const { value } = formDefaults;
+
+      const nodeStyle = await getNodeStyleFromReduxStore();
+      const { id, range, variable } = nodeStyle.size as NodeSizeProperty;
+      expect(id).to.deep.equal(nodeSizeType);
+      expect(variable).to.deep.equal('risk_score');
+      expect(range).to.deep.equal(value);
+    });
+  });
+
+  describe('Edge Width', () => {
+    const changeWidthType = (type: string) => {
+      cy.react('Controller', { props: { name: 'width' } }).type(
+        `${type}{enter}`,
+      );
+    };
+
+    const changeWidthVariable = (variable: string) => {
+      cy.react('Controller', { props: { name: 'variable' } })
+        .nthNode(1)
+        .type(`${variable}{enter}`);
+    };
+
+    it('Fixed', async () => {
+      changeWidthType('fixed');
+
+      const modifyValue = 1;
+      const arrows = '{rightarrow}'.repeat(modifyValue);
+      const edgeSizeType = 'fixed';
+      const controllerName = 'value';
+
+      cy.react('SettingsPopover')
+        .react('NestedForm')
+        .react('Controller', { props: { name: controllerName } })
+        .nthNode(1)
+        .type(arrows);
+
+      const { min, max } = findDefaultFromForm(
+        form.edgeWidthForm,
+        edgeSizeType,
+        controllerName,
+      );
+
+      const edgeStyle = await getEdgeStyleFromReduxStore();
+      const { id, value } = edgeStyle.width as EdgeWidthFixed;
+      expect(id).to.deep.equal(edgeSizeType);
+      expect(value).to.deep.equal((min + max) / 2 + 0.1);
+    });
+
+    it('Property (User Defined)', async () => {
+      const edgeSizeType = 'property';
+      const selectedVariable = 'is_foreign_source';
+      const controllerName = 'range';
+
+      changeWidthType('Property');
+      changeWidthVariable(selectedVariable);
+
+      const { value } = findDefaultFromForm(
+        form.edgeWidthForm,
+        edgeSizeType,
+        controllerName,
+      );
+
+      const edgeStyle = await getEdgeStyleFromReduxStore();
+      const { id, range, variable } = edgeStyle.width as EdgeWidthProperty;
+      expect(id).to.deep.equal(edgeSizeType);
+      expect(variable).to.deep.equal(selectedVariable);
+      expect(range).to.deep.equal(value);
+    });
+  });
 });
