7584c322795ff7d83ccbebd478509c27e7c5038d
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-13 16:04:29 +0800

    fix: prettier rules

--- packages/motif/src/redux/graph/index.ts
+++ packages/motif/src/redux/graph/index.ts
@@ -3,8 +3,8 @@ import reducer from './slice';
 // eslint-disable-next-line prettier/prettier
 export * as GraphSlices from './slice';
 export * as GraphThunks from './thunk';
-export * as GraphSelectors from "./selectors";
-export * as GraphUtils from "../../utils/graph-utils/utils";
-export * as GraphGroupEdgeProcessors from "./processors/group-edges";
-export * from "./types";
+export * as GraphSelectors from './selectors';
+export * as GraphUtils from '../../utils/graph-utils/utils';
+export * as GraphGroupEdgeProcessors from './processors/group-edges';
+export * from './types';
 export default reducer;
