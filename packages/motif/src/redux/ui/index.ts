802fafab92cd3927d28fc3d8d7c3e100e2269da3
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-13 16:00:27 +0800

    fix: prettier problems

--- packages/motif/src/redux/ui/index.ts
+++ packages/motif/src/redux/ui/index.ts
@@ -1,9 +1,8 @@
-import reducer from "./slice";
+import reducer from './slice';
 
-// eslint-disable-next-line prettier/prettier
-export * as UIThunks from "./thunks";
-export * as UISlices from "./slice";
-export * as UISelectors from "./selectors";
-export * as UIConstants from "./constants";
-export * from "./types";
+export * as UIThunks from './thunks';
+export * as UISlices from './slice';
+export * as UISelectors from './selectors';
+export * as UIConstants from './constants';
+export * from './types';
 export default reducer;
