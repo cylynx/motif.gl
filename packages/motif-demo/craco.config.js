b6cd1dbe21d4852f57cf634e7f50cc828eabe913
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-16 13:42:58 +0800

    chore: webpack alias configuration

---
+++ packages/motif-demo/craco.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const AliasPlugin = require('enhanced-resolve/lib/AliasPlugin');
+
+module.exports = {
+  webpack: {
+    configure: {
+      resolve: {
+        plugins: [
+          new AliasPlugin(
+            'described-resolve',
+            [
+              {
+                name: 'react',
+                alias: [
+                  path.resolve(__dirname, './node_modules/react'),
+                  path.resolve(__dirname, '../node_modules/react'),
+                ],
+              },
+              {
+                name: 'react-dom',
+                alias: [
+                  path.resolve(__dirname, './node_modules/react-dom'),
+                  path.resolve(__dirname, '../node_modules/react-dom'),
+                ],
+              },
+            ],
+            'resolve',
+          ),
+        ],
+      },
+    },
+  },
+};
