b290d827f1068289b16dbbb9118fe7a386bcd0f4
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-22 08:39:46 +0800

    chore: development compilations

--- packages/motif-demo/vite.config.js
+++ packages/motif-demo/vite.config.js
@@ -4,4 +4,7 @@ import { defineConfig } from 'vite';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
+  optimizeDeps: {
+    keepNames: true,
+  },
 });
--- packages/motif/vite/development.config.ts
+++ packages/motif/vite/development.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import postcss from 'rollup-plugin-postcss';
 import commonjs from '@rollup/plugin-commonjs';
 import url from '@rollup/plugin-url';
+import svgr from '@svgr/rollup';
 
 const libEntryPath = path.resolve(__dirname, '../src/index.tsx');
 const outputDir = path.resolve(__dirname, '../dist');
@@ -15,11 +16,23 @@ const commonJsPlugin = commonjs({
 }) as Plugin;
 const urlPlugin = url();
 
+// @ts-ignore
+const svgrPlugin: Plugin = svgr({
+  ref: true,
+  memo: true,
+  svgoConfig: {
+    plugins: [
+      { removeViewBox: false },
+      { removeAttrs: { attrs: 'g:(stroke|fill):((?!^none$).)*' } },
+    ],
+  },
+});
+
 export default defineConfig({
   mode: 'development',
   logLevel: 'info',
   clearScreen: false,
-  plugins: [postCssPlugin, commonJsPlugin, urlPlugin],
+  plugins: [postCssPlugin, commonJsPlugin, urlPlugin, svgrPlugin],
   build: {
     outDir: outputDir,
     sourcemap: true,
