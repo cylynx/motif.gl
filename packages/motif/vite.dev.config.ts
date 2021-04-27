b04c72218d12b6090cad74e6c8ec4979bacf6ddb
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-27 09:27:52 +0800

    chore: development build

--- packages/motif/vite.dev.config.ts
+++ packages/motif/vite.dev.config.ts
@@ -1,15 +1,6 @@
 import { defineConfig } from 'vite';
-import path from 'path';
-import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
-import ts from 'typescript';
 import reactRefresh from '@vitejs/plugin-react-refresh';
-import typescript from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel';
-
-const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
-
-const urlPlugin = url();
 
 const svgrPlugin = svgr({
   ref: true,
@@ -24,24 +15,9 @@ const svgrPlugin = svgr({
 
 const reactRefreshPlugin = reactRefresh();
 
-const typescriptPlugin = typescript({
-  typescript: ts,
-  tsconfig: tsconfigPath,
-}) as Plugin;
-
-const babelPlugin = babel({
-  babelHelpers: 'bundled',
-  exclude: 'node_modules/**',
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-    '@babel/preset-typescript',
-  ],
-}) as Plugin;
-
 export default defineConfig({
   mode: 'development',
   logLevel: 'info',
   clearScreen: false,
-  plugins: [typescriptPlugin, svgrPlugin],
+  plugins: [reactRefreshPlugin, svgrPlugin],
 });
