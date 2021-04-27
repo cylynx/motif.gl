7e9e6e6ec08780eee35bef5ba1750330fa4356b7
Yinghua Chai <yinghuachai0@gmail.com>
2021-04-27 11:19:42 +0800

    build: complete transpilation for production

--- packages/motif/vite.prod.config.ts
+++ packages/motif/vite.prod.config.ts
@@ -1,16 +1,11 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import postcss from 'rollup-plugin-postcss';
-import commonjs from '@rollup/plugin-commonjs';
 import url from '@rollup/plugin-url';
 import svgr from '@svgr/rollup';
-import ts from 'typescript';
-import typescript from 'rollup-plugin-typescript2';
-import babel from '@rollup/plugin-babel';
 
 const libEntryPath = path.resolve(__dirname, 'src/index.ts');
 const outputDir = path.resolve(__dirname, 'dist');
-const tsconfigPath = path.resolve(__dirname, 'tsconfig.json');
 const excludeDir = 'node_modules/**';
 
 const urlPlugin = url();
@@ -19,11 +14,6 @@ const postCssPlugin = postcss({
   extract: false,
   modules: false,
   minimize: false,
-}) as Plugin;
-
-const commonJsPlugin = commonjs({
-  exclude: excludeDir,
-  sourceMap: false,
 }) as Plugin;
 
 // @ts-ignore
@@ -38,33 +28,11 @@ const svgrPlugin: Plugin = svgr({
   },
 });
 
-const typescriptPlugin = typescript({
-  typescript: ts,
-  tsconfig: tsconfigPath,
-  clean: true,
-}) as Plugin;
-
-// const babelPlugin = babel({
-//   babelHelpers: 'bundled',
-//   exclude: 'node_modules/**',
-//   presets: [
-//     '@babel/preset-env',
-//     '@babel/preset-react',
-//     '@babel/preset-typescript',
-//   ],
-// });
-
 export default defineConfig({
   mode: 'production',
   logLevel: 'info',
   clearScreen: false,
-  plugins: [
-    commonJsPlugin,
-    typescriptPlugin,
-    urlPlugin,
-    postCssPlugin,
-    svgrPlugin,
-  ],
+  plugins: [urlPlugin, postCssPlugin, svgrPlugin],
   build: {
     outDir: outputDir,
     sourcemap: false,
