339380dd7f9284eafbe5706cc3c3f8853991c3aa
Yinghua Chai <yinghuachai0@gmail.com>
2021-05-05 10:49:21 +0800

    test: recover specific test

--- packages/motif/cypress/integration/ImportSampleData/random-circle.spec.ts
+++ packages/motif/cypress/integration/ImportSampleData/random-circle.spec.ts
@@ -12,7 +12,7 @@ describe('Import Random + Circle', () => {
     cy.switchTab('sample-data');
 
     // import sample data by clicking random graph
-    cy.importSampleData(SampleData.CIRCLE_GRAPH);
+    cy.importSampleData(SampleData.RANDOM_GRAPH);
 
     // waiting for loading indicator to dissappear
     cy.wait(500);
