0742dfc099ff00c8fe9e860d6bcd958360329833
unknown <jel.lim99@gmail.com>
2021-05-14 11:03:10 +0800

    style: add big and small variants for dropdown

--- packages/motif/src/components/ui/Dropdown.tsx
+++ packages/motif/src/components/ui/Dropdown.tsx
@@ -1,63 +1,37 @@
 import React, { FC } from 'react';
-import {
-  OnChangeParams,
-  Value,
-  Select,
-  SelectProps,
-  TYPE,
-} from 'baseui/select';
+import { OnChangeParams, Value, Select, SelectProps } from 'baseui/select';
 import * as Icon from '../Icons';
 
-export enum DropdownFontSize {
-  SMALL,
-  BIG,
-}
-
 type DropdownProps = SelectProps & {
-  disabled?: boolean;
   transparent?: boolean;
-  fontSize: DropdownFontSize;
   onChange: (params: OnChangeParams) => any;
   value: Value;
-  type?: TYPE[keyof TYPE];
   isOpen?: boolean;
+  fontSize: string | number;
+  popoverOverride: any;
 };
 
-export const RegularDropdown: FC<DropdownProps> = ({
-  disabled,
+export const Dropdown: FC<DropdownProps> = ({
   transparent,
-  fontSize,
   onChange,
   value,
-  type = TYPE.select,
+  fontSize,
+  popoverOverride,
+  ...rest
 }) => {
   return (
     <Select
       placeholder='Select'
-      disabled={disabled}
       onChange={onChange}
       value={value}
-      type={type}
-      options={[
-        { label: 'AliceBlue', id: '#F0F8FF' },
-        { label: 'AntiqueWhite', id: '#FAEBD7' },
-        { label: 'Aqua', id: '#00FFFF' },
-        { label: 'Aquamarine', id: '#7FFFD4' },
-        { label: 'Azure', id: '#F0FFFF' },
-        { label: 'Beige', id: '#F5F5DC' },
-      ]}
       overrides={{
         ControlContainer: {
           style: () => {
             return {
-              backgroundColor: transparent ? '#32323A' : 'none',
-            };
-          },
-        },
-        Placeholder: {
-          style: () => {
-            return {
-              fontSize: fontSize === DropdownFontSize.SMALL ? '14px' : '16px',
+              borderColor: transparent ? 'transparent' : '#8C8C97',
+              backgroundColor: transparent ? 'transparent' : '#32323A',
+              borderRadius: '6px',
+              fontSize,
             };
           },
         },
@@ -66,7 +40,16 @@ export const RegularDropdown: FC<DropdownProps> = ({
             return $isOpen ? <Icon.ChevronUp /> : <Icon.ChevronDown />;
           },
         },
+        Popover: popoverOverride,
+        DropdownListItem: {
+          style: () => {
+            return {
+              fontSize,
+            };
+          },
+        },
       }}
+      {...rest}
     />
   );
 };
