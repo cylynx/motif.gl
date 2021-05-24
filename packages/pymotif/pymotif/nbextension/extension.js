e2c4aad97da608bf1f26cf351a944917dc3da07b
Timothy Lin <timothy.lin@cylynx.io>
2021-05-24 09:56:40 +0800

    fix: jupyter notebook extension mapping

--- packages/pymotif/pymotif/nbextension/extension.js
+++ packages/pymotif/pymotif/nbextension/extension.js
@@ -6,7 +6,7 @@ define(function() {
     window['requirejs'].config({
         map: {
             '*': {
-                'pymotif': 'nbextensions/pymotif/index',
+                '@cylynx/pymotif': 'nbextensions/pymotif/index',
             },
         }
     });
