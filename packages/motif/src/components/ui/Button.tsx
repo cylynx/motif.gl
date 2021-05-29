2945ae811b52192f19e45d46d845d1468fe81ba6
Timothy Lin <timothy.lin@cylynx.io>
2021-05-29 11:13:59 +0800

    fix: forward ref

--- packages/motif/src/components/ui/Button.tsx
+++ packages/motif/src/components/ui/Button.tsx
@@ -1,48 +1,52 @@
 import React, { FC } from 'react';
-import { Button as BaseButton, SHAPE, ButtonProps } from 'baseui/button';
+import {
+  Button as BaseButton,
+  SHAPE,
+  ButtonProps as BaseButtonProps,
+} from 'baseui/button';
 
-type BorderButtonProps = ButtonProps & {
+type ButtonProps = BaseButtonProps & {
   children: React.ReactNode;
   width?: string;
   border?: 'solid' | 'none';
 };
 
-const Button: FC<BorderButtonProps> = ({
-  children,
-  width = 'auto',
-  border = 'none',
-  ...rest
-}) => {
-  return (
-    <BaseButton
-      {...rest}
-      size='compact'
-      overrides={{
-        BaseButton: {
-          style: ({ $theme, $shape }) => {
-            return {
-              borderColor: $theme.colors.accent500,
-              borderTopStyle: border,
-              borderBottomStyle: border,
-              borderLeftStyle: border,
-              borderRightStyle: border,
-              borderTopWidth: '1px',
-              borderBottomWidth: '1px',
-              borderLeftWidth: '1px',
-              borderRightWidth: '1px',
-              height: '32px',
-              width:
-                $shape === SHAPE.circle || $shape === SHAPE.square
-                  ? '32px'
-                  : width,
-            };
+type Ref = HTMLButtonElement;
+
+const Button = React.forwardRef<Ref, ButtonProps>(
+  ({ children, width = 'auto', border = 'none', ...rest }, ref) => {
+    return (
+      <BaseButton
+        ref={ref}
+        {...rest}
+        size='compact'
+        overrides={{
+          BaseButton: {
+            style: ({ $theme, $shape }) => {
+              return {
+                borderColor: $theme.colors.accent500,
+                borderTopStyle: border,
+                borderBottomStyle: border,
+                borderLeftStyle: border,
+                borderRightStyle: border,
+                borderTopWidth: '1px',
+                borderBottomWidth: '1px',
+                borderLeftWidth: '1px',
+                borderRightWidth: '1px',
+                height: '32px',
+                width:
+                  $shape === SHAPE.circle || $shape === SHAPE.square
+                    ? '32px'
+                    : width,
+              };
+            },
           },
-        },
-      }}
-    >
-      {children}
-    </BaseButton>
-  );
-};
+        }}
+      >
+        {children}
+      </BaseButton>
+    );
+  },
+);
 
 export default Button;
