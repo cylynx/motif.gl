0cb1309e42cca89df56ea662ea18b70c02bfc93c
Timothy Lin <timothy.lin@cylynx.io>
2021-06-02 16:49:55 +0800

    test: restore tests

--- packages/motif/cypress/integration/Toolbar/settings-popover.spec.ts
+++ packages/motif/cypress/integration/Toolbar/settings-popover.spec.ts
@@ -40,7 +40,7 @@ describe('Setting Popovers', () => {
 
   const getNodeStyleFromReduxStore = (): Promise<NodeStyleOptions> => {
     return new Promise((resolve) => {
-      cy.getReact('Provider')
+      cy.getReact('Provider$1')
         .getProps('store')
         .then((store) => {
           const globalStore = store.getState();
