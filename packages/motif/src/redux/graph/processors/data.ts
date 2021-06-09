ca5858949736355b1550f29b54852d7e0990e35f
Yinghua Chai <yinghuachai0@gmail.com>
2021-06-09 14:55:41 +0800

    fix: reject missing column header in csv and display error

--- packages/motif/src/redux/graph/processors/data.ts
+++ packages/motif/src/redux/graph/processors/data.ts
@@ -401,7 +401,9 @@ export const processCsvData = async (rawCsv: string): Promise<ProcessedCsv> => {
   const headerRow = rawCsv.replace(/\r/g, '').split('\n')[0].split(',');
 
   if (!parsedJson || !headerRow) {
-    throw new Error('invalid input passed to process Csv data');
+    throw new Error(
+      'Missing column headers in the top rows of uploaded CSV data',
+    );
   }
 
   // assume the csv file that uploaded csv will have first row
