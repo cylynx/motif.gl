cc49b18310088a8d8a78f85f544d2bd8c83a567c
Timothy Lin <timothy.lin@cylynx.io>
2021-06-14 12:15:39 +0800

    fix: update types error in ConfigureField

--- packages/motif/src/containers/ImportWizardModal/UploadFiles/ConfigureFields/AccessorFields.tsx
+++ packages/motif/src/containers/ImportWizardModal/UploadFiles/ConfigureFields/AccessorFields.tsx
@@ -5,6 +5,7 @@ import {
   Control,
   UseFormClearErrors,
   UseFormSetValue,
+  UseFormSetError
 } from 'react-hook-form';
 import { OnChangeParams, Option, Value } from 'baseui/select';
 import useFileContents, { defaultField } from '../hooks/useFileContents';
@@ -21,15 +22,7 @@ type AccessorsFieldsProps = {
   onSelectChange: (params: OnChangeParams, onChange: any) => any;
   control: Control<ConfigureFieldsForm>;
   watch: (names?: string | string[]) => any;
-  setError: (
-    name: keyof ConfigureFieldsForm,
-    error: {
-      type?: string;
-      types?: object;
-      message?: string;
-      shouldFocus?: boolean;
-    },
-  ) => void;
+  setError: UseFormSetError<ConfigureFieldsForm>;
   clearErrors: UseFormClearErrors<ConfigureFieldsForm>;
   getValues: (payload?: string | string[]) => Object;
   errors: Record<string, Object>;
