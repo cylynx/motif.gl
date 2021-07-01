b94c35ee27ac3b389994548858cb9341f28acc6a
Jason <jason.ho@lynxanalytics.com>
2021-07-01 15:07:59 +0800

    test: added remaining tests for pymotif

--- packages/pymotif/pymotif/tests/README.md
+++ packages/pymotif/pymotif/tests/README.md
@@ -1,2 +1,10 @@
-- install pytest and nbval
-- run `pytest`
+# Running tests
+
+```
+pip install pytest nbval
+pytest -v
+```
+
+`pytest -v` has to be run in this directory. Otherwise, specify it like this:
+
+`pytest <PATH TO THIS TEST DIR>/ -v`
--- packages/pymotif/pymotif/tests/test_motif.py
+++ packages/pymotif/pymotif/tests/test_motif.py
@@ -14,14 +14,14 @@ from .data.nx import create_nx_test_graph
 
 
 def test_initial_empty_state():
-    """ Data and style should be empty when nothing is passed """
+    ''' Data and style should be empty when nothing is passed '''
     motif = Motif()
 
     assert motif.state == { C.DATA: [], C.STYLE: {} }
 
 
 def test_json_import():
-    """ JSON test file """
+    ''' JSON test file '''
     motif = Motif(json_path=TC.TEST_JSON_PATH)
 
     with open(TC.TEST_JSON_PATH, 'r') as f:
@@ -31,67 +31,107 @@ def test_json_import():
 
 
 def test_2_col_csv_import():
-    """ CSV test file that only has 'source' and 'target' cols """
+    ''' CSV test file that only has 'source' and 'target' cols '''
     motif = Motif(csv_path=TC.TEST_2_COLS_CSV_PATH)
+    motif_graph_list = motif.state[C.DATA]
 
-    assert _motif_obj_has_expected_test_structure(motif)
+    # only 1 graph, which has expected test structure
+    assert len(motif_graph_list) == 1
+    assert _motif_graph_has_expected_test_structure(motif_graph_list[0])
 
 
 def test_gt_2_col_csv_import():
-    """ CSV test file that has cols other than 'source' and 'target' """
+    ''' CSV test file that has cols other than 'source' and 'target' '''
     motif = Motif(csv_path=TC.TEST_GT_2_COLS_CSV_PATH)
+    motif_graph_list = motif.state[C.DATA]
 
-    assert _motif_obj_has_expected_test_structure(motif)
+    # only 1 graph, which has expected test structure
+    assert len(motif_graph_list) == 1
+    assert _motif_graph_has_expected_test_structure(motif_graph_list[0])
 
 
 def test_nx_import():
-    """ NetworkX test graph """
+    ''' NetworkX test graph '''
     motif = Motif(nx_graph=create_nx_test_graph())
+    motif_graph_list = motif.state[C.DATA]
 
-    assert _motif_obj_has_expected_test_structure(motif)
+    # only 1 graph, which has expected test structure
+    assert len(motif_graph_list) == 1
+    assert _motif_graph_has_expected_test_structure(motif_graph_list[0])
 
 
 def test_neo4j_import():
-    """ Neo4j test graph """
+    ''' Neo4j test graph '''
     motif = Motif(neo4j_graph=create_neo4j_test_graph())
+    motif_graph_list = motif.state[C.DATA]
 
-    assert _motif_obj_has_expected_test_structure(motif)
+    # only 1 graph, which has expected test structure
+    assert len(motif_graph_list) == 1
+    assert _motif_graph_has_expected_test_structure(motif_graph_list[0])
 
 
 def test_import_style():
-    """  """
-    # TODO
+    ''' Set styles upon import  '''
+    motif = Motif(csv_path=TC.TEST_2_COLS_CSV_PATH, style=TC.TEST_STYLE_0)
+
+    assert motif.state[C.STYLE] == TC.TEST_STYLE_0
 
 
 def test_import_title():
-    """  """
-    # TODO
+    ''' Set title upon import '''
+    motif = Motif(csv_path=TC.TEST_2_COLS_CSV_PATH, title=TC.TEST_TITLE)
+    motif_graph = motif.state[C.DATA][0]
+
+    assert motif_graph['metadata']['title'] == TC.TEST_TITLE
 
 
 def test_add_graph():
-    """  """
-    # TODO
+    ''' Adding a new graph to an existing one '''
+    motif = Motif(csv_path=TC.TEST_GT_2_COLS_CSV_PATH)
+    motif.add_graph(nx_graph=create_nx_test_graph())
+    motif_graph_list = motif.state[C.DATA]
+
+    # 2 graphs, both of which have expected test structure
+    assert len(motif_graph_list) == 2
+
+    for motif_graph in motif_graph_list:
+        assert _motif_graph_has_expected_test_structure(motif_graph)
 
 
-def test_set_style():
-    """  """
-    # TODO
+def test_add_graph_style_overwrites_old_style():
+    ''' New style passed when adding graph overwrites old style '''
+    motif = Motif(csv_path=TC.TEST_GT_2_COLS_CSV_PATH, style=TC.TEST_STYLE_0)
+    motif.add_graph(nx_graph=create_nx_test_graph(), style=TC.TEST_STYLE_1)
+
+    assert motif.state[C.STYLE] == TC.TEST_STYLE_1
 
 
-def _motif_obj_has_expected_test_structure(motif_obj: Motif) -> bool:
-    """ 
-    Checks that a motif class instance has the expected test structure:
-        - only 1 graph
-        - graph has 2 nodes that have at least an 'id' key
-        - graph has 1 edge that has at least a 'source' and 'target' col with the 2 nodes above
-    """
-    # extract list of graphs
-    motif_graph_list = motif_obj.state[C.DATA]
+def test_set_style_with_overwrite():
+    ''' Set style of an existing graph, overwriting old style '''
+    motif = Motif(csv_path=TC.TEST_GT_2_COLS_CSV_PATH, style=TC.TEST_STYLE_0)
+    motif.set_style(style=TC.TEST_STYLE_1, overwrite=True)
 
-    assert len(motif_graph_list) == 1
+    assert motif.state[C.STYLE] == TC.TEST_STYLE_1
 
-    nodes = motif_graph_list[0][C.NODES]
-    edges = motif_graph_list[0][C.EDGES]
+
+def test_set_style_without_overwrite():
+    ''' Set style of an existing graph, merging with old styles '''
+    motif = Motif(csv_path=TC.TEST_GT_2_COLS_CSV_PATH, style=TC.TEST_STYLE_0)
+    motif.set_style(style=TC.TEST_STYLE_1, overwrite=False)
+    merged_style = {**TC.TEST_STYLE_0, **TC.TEST_STYLE_1}
+
+    assert motif.state[C.STYLE] == merged_style
+
+
+def _motif_graph_has_expected_test_structure(motif_graph: Motif) -> bool:
+    ''' 
+    Checks that a motif graph has the expected test structure:
+        - 2 nodes that have at least an 'id' key
+        - 1 edge that has at least a 'source' and 'target' col with the 2 nodes above
+    '''
+
+    nodes = motif_graph[C.NODES]
+    edges = motif_graph[C.EDGES]
 
     assert len(nodes) == 2
     assert len(edges) == 1
