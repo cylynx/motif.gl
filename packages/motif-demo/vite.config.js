e53c00a62b199c7032def05d6fe85b8d092a0b3f
Timothy Lin <timothy.lin@cylynx.io>
2021-07-21 13:50:31 +0800

    chore: update graphin to 2.1.0 and vite to 2.4.3

--- packages/motif-demo/vite.config.js
+++ packages/motif-demo/vite.config.js
@@ -5,6 +5,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   plugins: [reactRefresh()],
   optimizeDeps: {
-    keepNames: true,
+    esbuildOptions: {
+      keepNames: true,
+    },
   },
 });
