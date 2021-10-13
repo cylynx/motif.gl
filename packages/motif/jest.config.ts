14e1451aa9c84490f11339d0e191ce92979d11dd
Yinghua Chai <yinghuachai0@gmail.com>
2021-10-13 12:57:40 +0800

    build: configure jest to transform es module for d3-quadtree

--- packages/motif/jest.config.ts
+++ packages/motif/jest.config.ts
@@ -165,11 +165,13 @@ export default {
 
   // A map from regular expressions to paths to transformers
   transform: {
-    '/node_modules/lodash-es/.+\\.(j|t)sx?$': 'ts-jest',
+    '/node_modules/(lodash-es|d3-quadtree)/.+\\.(j|t)sx?$': 'ts-jest',
   },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
-  transformIgnorePatterns: ['/node_modules/(?!lodash-es|@cylynx/graphin)'],
+  transformIgnorePatterns: [
+    '<rootDir>/node_modules/?!(lodash-es|@cylynx/graphin)',
+  ],
 
   // An array of regexp pattern strings that are matched against all modules before the module loader will automatically return a mock for them
   // unmockedModulePathPatterns: undefined,
